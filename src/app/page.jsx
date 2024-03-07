cae050cf860306ff247e9e168c48b19d62c86d26
Prince <128713671+ft-prince@users.noreply.github.com>
2024-03-07 20:43:11 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import { motion } from "framer-motion";
 
@@ -9,22 +7,22 @@ const Homepage = () => {
       className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
-      transition={{ duration: 1 }}>
+      transition={{ duration: 1 }}
+    >
       <div className="h-full flex flex-col lg:flex-row px-5 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative mb-24">
-          <Image src="/Prince.png" alt="Portrait of Prince" fill className="object-contain" />
-          
+          <Image src="/Prince.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-          Igniting Digital Adventures: Crafting Tomorrow's Experiences
+            Igniting Digital Adventures: Crafting Tomorrow&apos;s Experiences
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Welcome to the realm of digital exploration! I'm a passionate developer on a quest to push boundaries and craft unforgettable experiences. Dive into my portfolio to discover a world where imagination knows no bounds and every project is an exciting journey into the future. Let's shape tomorrow's digital landscape together with ingenuity and flair!
+            Welcome to the realm of digital exploration! I&apos;m a passionate developer on a quest to push boundaries and craft unforgettable experiences. Dive into my portfolio to discover a world where imagination knows no bounds and every project is an exciting journey into the future. Let&apos;s shape tomorrow&apos;s digital landscape together with ingenuity and flair!
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
