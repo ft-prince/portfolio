bb6edd54fd61947aa7886f6da60b9ef1be8bba1a
Prince <128713671+ft-prince@users.noreply.github.com>
2024-03-07 20:17:20 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,8 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-5 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative mb-24">
-          <Image src="/Prince.png" alt="" fill className="object-contain" />
+          <Image src="/Prince.png" alt="Portrait of Prince" fill className="object-contain" />
+          
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-6 items-center justify-center">
